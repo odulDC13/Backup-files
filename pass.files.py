5c8aa656ceb885ec259597aa4e9aedccd835bdfa
Akira <142701354+odulDC13@users.noreply.github.com>
2025-07-10 20:45:56 -0500

    Update pass.files.py

--- pass.files.py
+++ pass.files.py
@@ -1,21 +1,21 @@
 import os
 import shutil
+import sys
+
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
 
 def copy_files(source_folder, destination_folder):
-    # Verificar si la carpeta de destino existe, si no, crearla
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
-    # Recorrer todos los archivos y directorios en la carpeta de origen
     for root, dirs, files in os.walk(source_folder):
-        # Construir la ruta de destino correspondiente a la carpeta actual
         destination_root = root.replace(source_folder, destination_folder, 1)
-
-        # Verificar si la carpeta de destino existe, si no, crearla
         if not os.path.exists(destination_root):
             os.makedirs(destination_root)
-
-        # Copiar todos los archivos en la carpeta actual a la carpeta de destino
         for file in files:
             source_path = os.path.join(root, file)
             destination_path = os.path.join(destination_root, file)
@@ -23,12 +23,8 @@ def copy_files(source_folder, destination_folder):
 
     print("Archivos copiados correctamente.")
 
-# Carpeta de origen (Documentos del usuario actual)
 source_folder = os.path.join(os.environ['USERPROFILE'], 'Documents')
-
-# Carpeta de destino (relativa a la ubicación del script)
-script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = get_base_path()
 destination_folder = os.path.join(script_dir, 'files')
 
-# Llamar a la función para copiar los archivos
 copy_files(source_folder, destination_folder)
